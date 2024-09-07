d0b460de6b0e4c5c6c8674f1a0bec4196ea2fa73
Aline09 <oliveira.alineod@gmail.com>
2024-09-07 19:36:40 -0300

    feat: create test using unittest

---
+++ test_category.py
@@ -0,0 +1,52 @@
+import unittest
+from uuid import UUID, uuid4 # std library  - ja vem instalado com o python
+
+from category import Category
+
+class TestCategory(unittest.TestCase):
+    def test_name_is_required(self):
+        with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'name'"):
+            Category()
+
+    def test_name_must_have_less_than_255_characters(self):
+        with self.assertRaisesRegex(ValueError, "name must have less than 256 characteres"): # não lancou o value error
+            Category(name = "a" * 256)
+    
+    def test_category_must_be_create_with_id_as_uuid(self):
+            category = Category(name="Filme")
+            self.assertEqual(type(category.id), UUID)
+
+    def test_created_category_with_default_values(self):
+         category = Category(name="Filme")
+         self.assertEquals(category.name, "Filme")
+         self.assertEquals(category.description, "")
+         self.assertEquals(category.is_active, True)
+
+    def test_category_is_created_as_active_by_default(self):
+         category = Category(name="Documentário")
+         self.assertEquals(category.is_active, True)
+    
+    def test_category_is_created_with_provided_values(self):
+         id = uuid4()
+         category = Category(name="Novelas")
+         category.id = id
+         category.description = "Novelas da Globo"
+         category.is_active = False
+
+         self.assertEquals(category.name, "Novelas")
+         self.assertEquals(category.description, "Novelas da Globo")
+         self.assertEquals(category.id, id)
+         self.assertEquals(category.is_active, False)
+
+    def test_show_category_as_string(self):
+         id = uuid4()
+         category = Category(name="Séries", id=id, description="Séries americanas")
+         self.assertEqual(str(category), f"{category.name} - {category.description} - ({category.is_active})")
+    
+    def test_show_category_representation(self):
+         id = uuid4()
+         category = Category(name="Séries", id=id)
+         self.assertEquals(repr(category), f"<Category {category.name} ({category.id})>" )
+
+if __name__ == "__main__":
+    unittest.main()
