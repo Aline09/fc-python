0388ebff885ece01667b9ceb9b36e340883d71f4
Aline09 <oliveira.alineod@gmail.com>
2024-09-07 19:36:49 -0300

    feat: create category

---
+++ category.py
@@ -0,0 +1,25 @@
+import uuid
+class Category:
+    # Tem que declarar o self dentro da função!
+    # No python os argumentos obrigatórios vem antes e os opcionais depois
+    def __init__(
+            self,
+            name,
+            id = "", 
+            description = "", 
+            is_active = True
+        ):
+        self.id = id or uuid.uuid4()
+        self.name = name
+        self.description = description
+        self.is_active = is_active
+    
+        if len(self.name) > 255:
+            raise ValueError("name must have less than 256 characteres")
+    # Toda a vez que eu tentar exibir um objeto dando um print ao invés de mostrar a classe e a posição de memória ele mostra algo mais semântico ao usuário
+    def __str__(self):
+        return f"{self.name} - {self.description} - ({self.is_active})"
+    
+    # Desse modo mesmo que eu não print o valor terei uma forma mais simples de ver os dados 
+    def __repr__(self):
+        return f"<Category {self.name} ({self.id})>" 
