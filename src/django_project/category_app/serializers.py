1fdee06d69181627e467406c7cdc262d27a48e05
Aline09 <oliveira.alineod@gmail.com>
2024-09-17 17:50:34 -0300

    feat: create serializers

---
+++ src/django_project/category_app/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+class CategoryResponseSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    name = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    is_active = serializers.BooleanField()
+
+class ListCategoryResponseSerializer(serializers.Serializer):
+    data = CategoryResponseSerializer(many=True)
+
+# Validar a requisição fora da view
+class RetrieveCategoryRequestSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+
+class RetrieveCategoryResponseSerializer(serializers.Serializer):
+    data = CategoryResponseSerializer(source="*")
+
+class CreateCategoryRequestSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    is_active = serializers.BooleanField(default=True)
+
+class CreateCategoryResponseSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
